27c04adb447bbf657f980ab1da98e901dc8bc71b
MunchingKitten <139931826+MunchingKitten@users.noreply.github.com>
2023-09-19 13:00:52 -0400

    Fixed some bugs! :D
    Found more bugs! D:

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
 let operator;
-let firstNum = null;
-let nextNum = null;
-let displayNum;
+let firstNum = "";
+let nextNum = "";
+let displayNum = "";
 
 const screen = document.querySelector('#display');
+
+const limit = (string) => {
+    return string.substring(0, 15);
+}
+
 const screenClear = () => {
     screen.textContent = "";
-    firstNum = 0;
+    firstNum = "";
     operator = null;
-    nextNum = 0;
+    nextNum = "";
 }
 
 const operate = (operator, num1, num2) => {
-        if (operator == 'add') return num1 + num2;
-        else if (operator =='sub') return num1 - num2;
-        else if (operator == 'multiply') return num1 * num2;
-        else if (operator == 'divide') return num1 / num2;
+        if (operator == 'add') return Number(num1 + num2);
+        else if (operator =='sub') return Number(num1 - num2);
+        else if (operator == 'multiply') return Number(num1 * num2);
+        else if (operator == 'divide') return Number(num1 / num2);
 } 
 
 const numBtns = document.querySelectorAll(".numBtn");
 numBtns.forEach((button) => {
     button.addEventListener('click', (e) => {
-        screen.textContent += e.target.textContent;
         if(firstNum && operator) {
-            nextNum = nextNum * 10 + Number(e.target.id);
-            screen.textContent = nextNum;
+            if(nextNum.includes(".") && e.target.id == ".") {
+                nextNum = nextNum;
+            }
+            else if (nextNum == "" && e.target.id == ".") {
+                nextNum = "0.";
+                screen.textContent = nextNum;
+            }
+        else {
+            if(nextNum.length >= 15) {
+                nextNum = limit(nextNum);
+                }
+            else {
+                nextNum += e.target.id;
+                screen.textContent = nextNum;
+                }
+            }
+
         }
         else if(operator == null) {
-            firstNum = firstNum * 10 + Number(e.target.id);
-            displayNum = firstNum;
+            if(firstNum.includes(".") && e.target.id == ".") {
+                firstNum = firstNum;
+            }
+            else if(firstNum == "" && e.target.id == ".") {
+                firstNum = "0.";
+                displayNum = firstNum;
+                screen.textContent = displayNum;
+            }
+            else{
+                if(firstNum.length >= 15) {
+                    firstNum = limit(firstNum);
+                }
+                else {
+                    firstNum += e.target.id;
+                    displayNum = firstNum;
+                    screen.textContent = displayNum;
+                }
+            }
         }
         console.log(`First: ${firstNum} 
         Next: ${nextNum}
@@ -41,23 +76,35 @@ numBtns.forEach((button) => {
 const mathBtns = document.querySelectorAll(".mathBtn");
 mathBtns.forEach((button) => {
     button.addEventListener('click', (e) => {
-        if(nextNum != null) {
-            displayNum = Number(operate(operator, displayNum, nextNum));
+        if(nextNum != "") {
+            displayNum = (operate(operator, Number(displayNum), Number(nextNum)));
+            if(`${displayNum}`.length >= 15) {
+                displayNum = limit(displayNum);
+                screen.textContent = displayNum;
+            }
+            else if(`${displayNum}`.length < 15) {
+                screen.textContent = displayNum;
+            }
         }
         operator = e.target.id;
-        nextNum = null;
-        screen.textContent = displayNum;
+        nextNum = "";
         console.log(`First: ${firstNum} Next: ${nextNum} Operator: ${operator} Display: ${displayNum}`);
         console.log(e.target.id);
+
     });
 });
 
 const equalsBtn = document.querySelector(".equalsBtn");
 equalsBtn.addEventListener('click', () => {
-    displayNum = Number(operate(operator, displayNum, nextNum));
+    if(screen.textContent != displayNum || screen.textContent == displayNum && nextNum == displayNum) {
+        displayNum = (operate(operator, Number(displayNum), Number(nextNum)));
+    }    
+    if(`${displayNum}`.length >= 15) {
+        displayNum = limit(displayNum.toString());
+    }
     screen.textContent = displayNum;
     operator = null;
-    nextNum = null;
+    nextNum = "";
     console.log(`First: ${firstNum} 
              Next: ${nextNum}
              Operator: ${operator}
